4f14037ea1966ee53b7aff3ffe802c81fda757b7
akashevomrf <akash@evomorf.com>
2025-02-04 13:01:28 +0530

    created custom logger for better understanding the request and res

--- backend/03_express_crash_course/index.js
+++ backend/03_express_crash_course/index.js
@@ -1,46 +1,65 @@
 import 'dotenv/config'
 import express from 'express'
+import logger from "./logger.js";
+import morgan from "morgan";
+
 
 const app = express();
 const port = process.env.PORT || 3000
 app.use(express.json())
 
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+    morgan(morganFormat, {
+        stream: {
+            write: (message) => {
+                const logObject = {
+                    method: message.split(" ")[0],
+                    url: message.split(" ")[1],
+                    status: message.split(" ")[2],
+                    responseTime: message.split(" ")[3],
+                };
+                logger.info(JSON.stringify(logObject));
+            },
+        },
+    })
+);
 
 const teaData = [] //to store the teas
 let id = 0;
 
 //create a tea
-app.post('/teas', (req, res)=>{
-    console.log(req.body)
-    const {name, price} = req.body
-    const newTea = {id:id++, name, price}
+app.post('/teas', (req, res) => {
+    logger.info('A post request made to add a new tea');
+    const { name, price } = req.body
+    const newTea = { id: id++, name, price }
     teaData.push(newTea)
     res.status(201).send(newTea)
 })
 
 // get all tea
-app.get('/teas', (req, res)=>{
+app.get('/teas', (req, res) => {
     res.status(200).send(teaData)
 })
 
 //find a tea using with id
-app.get('/teas/:id', (req,res)=>{
+app.get('/teas/:id', (req, res) => {
     const id = req.params.id
     const tea = teaData.find(t => t.id === parseInt(id))
-    if(!tea){
+    if (!tea) {
         return res.status(404).send("Couldn't find the tea")
     }
     res.status(200).send(tea)
 })
 
 //update tea
-app.put('/teas/:id', (req, res)=>{
+app.put('/teas/:id', (req, res) => {
     const id = req.params.id
     const tea = teaData.find(t => t.id === parseInt(id))
-    if(!tea){
+    if (!tea) {
         return res.status(404).send("Couldn't find the tea")
     }
-    const {name, price} = req.body;
+    const { name, price } = req.body;
     tea.name = name;
     tea.price = price;
     res.status(200).send(tea)
@@ -48,9 +67,9 @@ app.put('/teas/:id', (req, res)=>{
 
 
 //delete tea
-app.delete('/teas/:id', (req, res)=>{
+app.delete('/teas/:id', (req, res) => {
     const idx = teaData.findIndex(t => t.id === parseInt(req.params.id))
-    if(idx === -1){
+    if (idx === -1) {
         return res.status(404).send('tea not found')
     }
     teaData.splice(idx, 1)
