af87c7a67004191f9b25f3c145ff949ff3af739e
akashevomrf <akash@evomorf.com>
2025-02-04 13:00:55 +0530

    Dragable component

--- learning/src/components/About.jsx
+++ learning/src/components/About.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import transition from '../transition'
+import {motion} from 'motion/react'
 const About = () => {
-  return (
-    <section className='section-container about text-7xl ff-anton'>About</section>
-  )
+  const constraintsRef = useRef(null)
+
+    return (
+        <motion.div ref={constraintsRef} style={constraints}>
+            <motion.div
+                drag
+                dragConstraints={constraintsRef}
+                dragElastic={0.5}
+                style={box}
+            />
+        </motion.div>
+    )
+}
+const constraints = {
+  width: '100%',
+  height: '80%',
+  backgroundColor: "var(--hue-1-transparent)",
+  borderRadius: 10,
 }
 
+const box = {
+  width: 100,
+  height: 100,
+  backgroundColor: "#ff0088",
+  borderRadius: 10,
+}
 export default transition(About)
