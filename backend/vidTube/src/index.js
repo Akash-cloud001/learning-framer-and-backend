b228b95ffa825e0fe09218702a4afb913b040bdc
akashevomrf <akash@evomorf.com>
2025-02-06 17:26:17 +0530

    connected my application with mongoDb using Mongoose

--- backend/vidTube/src/index.js
+++ backend/vidTube/src/index.js
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv';
 import { app } from "./app.js";
+import connectDB from './db/index.js';
 
 dotenv.config({
     path: "./.env"
 })
 
 const PORT = process.env.PORT || 7000
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+
+connectDB().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`)
+    }) 
+}).catch((err)=>console.log('mongoDB connection error :: ', err))
