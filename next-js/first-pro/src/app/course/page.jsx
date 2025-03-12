df0d983d279e87eda88c9455186eaae52c52490f
akashevomrf <akash@evomorf.com>
2025-03-12 16:55:33 +0530

    understanding about dynamic routing in next app

--- next-js/first-pro/src/app/course/page.jsx
+++ next-js/first-pro/src/app/course/page.jsx
@@ -1,9 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <section className='w-full flex items-center justify-center'>
+    <section className='w-full flex flex-col gap-8 items-center justify-center'>
         Course Page
+        <ul>
+          <li><Link href={'/course/course-details/course-1'}>course-1</Link></li>
+          <li><Link href={'/course/course-details/course-2'}>course-2</Link></li>
+          <li><Link href={'/course/course-details/course-3'}>course-3</Link></li>
+        </ul>
     </section>
   )
 }
